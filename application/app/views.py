4b3c0242f416c4c1e8aa3f818c490a9e7da6c976
Vladimir <cay108@yandex.ru>
2023-08-16 23:47:11 +0300

    add plank views

--- application/app/views.py
+++ application/app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseNotAllowed
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        pass
+    else:
+        return HttpResponseNotAllowed()
+
+def authorization(request):
+    if request.method == 'POST':
+        pass
+    else:
+        return HttpResponseNotAllowed()
