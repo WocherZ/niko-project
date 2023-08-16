c7f9a835d6aebce9346aa6eb120080deab054208
Vladimir <cay108@yandex.ru>
2023-08-17 00:29:27 +0300

    create user model

--- application/app/models.py
+++ application/app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email = models.CharField(max_length=64)
+    name = models.CharField(max_length=128)
+    surname = models.CharField(max_length=128)
+    password = models.CharField(max_length=128)
+
+def create_user(email: str, name: str, surname: str, password: str):
+    user = User(email=email, name=name, surname=surname, password=password)
+    user.save()
+
+def is_exist_user_by_email(email: str) -> bool:
+    user_by_email = User.objects.filter(email=email)
+    if user_by_email.get(): return True
+    else: return False
+
+def is_correct_password_by_email(email: str, password: str) -> bool:
+    if is_exist_user_by_email(email):
+        user_by_email_password = User.objects.filter(email=email).filter(password=password)
+        if user_by_email_password.get(): return True
+        else: return False
+    else:
+        return False
+
