b8f5e4819378bb8806b53d34698ae9214b160888
Vladimir <cay108@yandex.ru>
2023-08-17 00:37:33 +0300

    add view logic

--- application/app/views.py
+++ application/app/views.py
@@ -1,14 +1,43 @@
+import json
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotAllowed
-# Create your views here.
+from django.http import HttpResponse, HttpResponseNotAllowed, HttpResponseBadRequest
+
+from .models import *
+
 def register(request):
     if request.method == 'POST':
-        pass
+        try:
+            data = json.loads(request.raw_post_data)
+            email = data.get('email')
+            name = data.get('name')
+            surname = data.get('surname')
+            password = data.get('password')
+
+            if not is_exist_user_by_email(email):
+                create_user(email, name, surname, password)
+                return HttpResponse("OK")
+            else:
+                return HttpResponseBadRequest()
+        except Exception as e:
+            print(e)
+            return HttpResponseBadRequest()
     else:
         return HttpResponseNotAllowed()
 
 def authorization(request):
     if request.method == 'POST':
-        pass
+        try:
+            data = json.loads(request.raw_post_data)
+            email = data.get('email')
+            password = data.get('password')
+
+            if is_correct_password_by_email(email, password):
+                return HttpResponse("OK")
+            else:
+                return HttpResponseBadRequest()
+
+        except Exception as e:
+            print(e)
+            return HttpResponseBadRequest()
     else:
         return HttpResponseNotAllowed()
